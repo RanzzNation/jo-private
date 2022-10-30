670588bdf35484631f176948695e5527085af6e5
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-30 09:20:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6289697155781','6289697155781','6289697155781']
+global.premium = ['6289697155781']
+global.packname = 'The Ranzz Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
